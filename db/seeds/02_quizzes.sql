8bc96ad5ccd84f2a5049c33399eecc3d2555fb76
EuphieSS <samma118@hotmail.com>
2023-02-12 21:04:06 +0000

    Add database version one

---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,5 @@
+INSERT INTO quizzes (creator_id, title, type, public, thumbnail_url)
+VALUES
+(2, 'Test your Covid Knowledge', 'Educational', true, 'placeholder'),
+(1, '8th Grade History Quiz', 'Educational', true, 'placeholder'),
+(3, 'The Ultimate Harry Potter Quiz', 'Trivia', true, 'placeholder');
