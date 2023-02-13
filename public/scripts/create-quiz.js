091f84a8d5f85dfdcf483724a5507b354b1f359a
Irha Ali <irhaali_97@hotmail.ca>
2023-02-13 12:21:34 -0500

    FInish fill out field required

--- public/scripts/create-quiz.js
+++ public/scripts/create-quiz.js
@@ -10,27 +10,27 @@ $(document).ready(() => {
     <fieldset class=q${questionNumber}>
     <div>
       <label for="q${questionNumber}">Question ${questionNumber}:</label><br>
-      <input type="text" id="q${questionNumber}" name="q${questionNumber}"><br>
+      <input type="text" id="q${questionNumber}" name="q${questionNumber}" required><br>
     </div>
     <div>
       <label for="q${questionNumber}a">A:</label><br>
-      <input type="text" id="q${questionNumber}a" name="q${questionNumber}a"><br>
+      <input type="text" id="q${questionNumber}a" name="q${questionNumber}a" required><br>
     </div>
     <div>
       <label for="q${questionNumber}b">B:</label><br>
-      <input type="text" id="q${questionNumber}b" name="q${questionNumber}b"><br>
+      <input type="text" id="q${questionNumber}b" name="q${questionNumber}b" required><br>
     </div>
     <div>
       <label for="q${questionNumber}c">C:</label><br>
-      <input type="text" id="q${questionNumber}c" name="q${questionNumber}c"><br>
+      <input type="text" id="q${questionNumber}c" name="q${questionNumber}c" required><br>
     </div>
     <div>
       <label for="q${questionNumber}d">D:</label><br>
-      <input type="text" id="q${questionNumber}d" name="q${questionNumber}d"><br>
+      <input type="text" id="q${questionNumber}d" name="q${questionNumber}d" required><br>
     </div>
     <div>
       <label for="q${questionNumber}-corr-ans">Correct Answer:</label><br>
-      <input type="text" id="q${questionNumber}-corr-ans" name="q${questionNumber}-corr-ans"><br>
+      <input type="text" id="q${questionNumber}-corr-ans" name="q${questionNumber}-corr-ans" required><br>
     </div>
   </fieldset>`;
     $(question).insertBefore(".all-buttons");
