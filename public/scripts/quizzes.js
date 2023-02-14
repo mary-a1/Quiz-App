0fc37ef8a24449c3b3513bfc68b231c45a160bcc
Maryan <maryanay2000@gmail.com>
2023-02-14 10:35:22 -0500

    Add route and db query for /quizzes

---
+++ public/scripts/quizzes.js
@@ -0,0 +1,24 @@
+$(() => {
+  // window.quizListing = {};
+
+  // function myQuizzes(quizzes) {
+  //   return `
+  //   <article class="my-quizzes">
+  //       <section class="my-quizzes-preview-image">
+  //         <img src="${quizzes.thumbnail_url}" alt="quiz">
+  //       </section>
+  //       <section class="quiz-details">
+  //         <h3 class="my-quiz-title">${quizzes.title}</h3>
+  //         <ul class="quiz-details">
+  //           <li>Type of Quiz: ${quizzes.type}</li>
+  //       </section>
+  //         <footer class="property-listing__footer">
+  //           <div class="quiz-listing-rating">${quiz_reviews.rating}/5 stars</div>
+  //         </footer>
+  //       </section>
+  //     </article>
+  //   `
+  // }
+  // window.quizListing = myQuizzes;
+
+});
--- server.js
+++ server.js
@@ -47,6 +47,7 @@ const searchRoutes = require('./routes/search');
 const createRoutes = require('./routes/create-quiz');
 const loginRoutes = require('./routes/login');
 const registerRoutes = require('./routes/register');
+const quizzesRoutes = require('./routes/quizzes');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -59,6 +60,8 @@ app.use('/search', searchRoutes);
 app.use('/quiz/new', createRoutes);
 app.use('/login', loginRoutes);
 app.use('/register', registerRoutes);
+app.use('/quizzes', quizzesRoutes);
+
 
 
 
