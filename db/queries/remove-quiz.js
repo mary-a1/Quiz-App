77d0f3ab45ba28ba0066159bb58a80c9cadd1155
Irha Ali <irhaali_97@hotmail.ca>
2023-02-14 15:39:19 -0500

    Complete delete quiz query

--- db/queries/remove-quiz.js
+++ db/queries/remove-quiz.js
@@ -2,9 +2,15 @@ const db = require('../connection');
 
 const removeQuiz = function(quizId) {
   const queryParams = [quizId];
-  const queryString = `
-  SELECT * FROM quizzes JOIN questions ON id = creator_id WHERE id = $1`;
-  return db.query(queryString, queryParams);
+  let queryString = `DELETE FROM questions WHERE id = $1;`;
+  return db.query(queryString, queryParams)
+    .then(() => {
+      queryString = `DELETE FROM quizzes WHERE id = $1;`;
+      return db.query(queryString, queryParams);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 };
 
 module.exports = { removeQuiz };
