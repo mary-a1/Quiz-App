4fe9b6eafe5b7ed8984be576d97d4279241b1ecf
Maryan <maryanay2000@gmail.com>
2023-02-14 15:23:11 -0500

    Adjust the function for adding question

--- public/scripts/create-quiz.js
+++ public/scripts/create-quiz.js
@@ -5,46 +5,45 @@ $(document).ready(() => {
   $('.add-question').on('click', function() {
     questionNumber++;
     const question = `
-    <fieldset class="questions">
+    <fieldset class="q" id="q${questionNumber}">
     <div>
-      <label for="q1">Question ${questionNumber}:</label><br>
+      <label for="q${questionNumber}">Question ${questionNumber}:</label><br>
       <textarea class="question-box" type="text" id="q${questionNumber}" name="q${questionNumber}" required></textarea>
     </div>
     <section class=answers-one>
-      <div>
-        <label for="q${questionNumber}a">1:</label><br>
-        <textarea class="text" type="text" id="q${questionNumber}a" name="q${questionNumber}a" required></textarea>
-      </div>
-      <div>
-        <label for="q${questionNumber}b">2:</label><br>
-        <textarea class="text" type="text" id="q${questionNumber}b" name="q${questionNumber}b" required></textarea>
-      </div>
-      <div>
-        <label for="q${questionNumber}c">3:</label><br>
-        <textarea class="text" type="text" id="q${questionNumber}c" name="q${questionNumber}c" required></textarea>
-      </div>
-      <div>
-        <label for="q${questionNumber}d">4:</label><br>
-        <textarea class="text" type="text" id="q${questionNumber}d" name="q${questionNumber}d" required></textarea>
-      </div>
+    <div>
+      <label for="q${questionNumber}a">1:</label><br>
+      <textarea class="text" type="text" id="q${questionNumber}a" name="q${questionNumber}a" required></textarea>
+    </div>
+    <div>
+      <label for="q${questionNumber}b">2:</label><br>
+      <textarea class="text"  type="text" id="q${questionNumber}b" name="q${questionNumber}b" required></textarea>
+    </div>
+    <div>
+      <label for="q${questionNumber}c">3:</label><br>
+      <textarea class="text" type="text" id="q${questionNumber}c" name="q${questionNumber}c" required></textarea>
+    </div>
+    <div>
+      <label for="q${questionNumber}d">4:</label><br>
+      <textarea class="text" type="text" id="q${questionNumber}d" name="q${questionNumber}d" required></textarea>
+    </div>
     </section>
     <div>
       <label for="q${questionNumber}-corr-ans">Correct Answer:</label><br>
-      <select name="question-one" id="question-one">
+      <select name="q${questionNumber}-select" id="q${questionNumber}-select">
         <option value="option-1">1</option>
         <option value="option-2">2</option>
         <option value="option-3">3</option>
         <option value="option-4">4</option>
       </select>
     </div>
-
   </fieldset>`;
     $(question).insertBefore(".all-buttons");
     $('.rem-question').css('display', 'block');
   });
 
   $('.rem-question').on('click', function() {
-    const $question = `.q${questionNumber}`;
+    const $question = `#q${questionNumber}`;
     $($question).remove();
     questionNumber--;
     $('.rem-question').css('display', (questionNumber > 4) ? 'block' : 'none');
