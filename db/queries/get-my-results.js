78f550660d5ef6f975804c6b7380ab6f52381474
Irha Ali <irhaali_97@hotmail.ca>
2023-02-15 18:00:33 -0500

    Finish my results page

---
+++ db/queries/get-my-results.js
@@ -0,0 +1,26 @@
+const db = require('../connection');
+
+const getAllMyResults = function(userId) {
+  const queryParams = [userId];
+  let queryString = `
+  SELECT COUNT(*) AS score, quizzes.title
+  FROM questions AS questions
+  JOIN results AS results
+  ON question_id = questions.id
+  JOIN quizzes AS quizzes
+  ON quiz_id = quizzes.id
+  WHERE results.chosen_answer = questions.correct_answer AND
+  results.player_id = $1
+  GROUP BY quizzes.id
+  ORDER BY score;`;
+
+  return db.query(queryString, queryParams)
+    .then((data) => {
+      return data.rows;
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+module.exports = { getAllMyResults };
