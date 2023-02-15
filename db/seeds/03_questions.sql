e10a2a15e0055d8845b9abf312cf9594b3ef347c
EuphieSS <samma118@hotmail.com>
2023-02-15 20:27:01 +0000

    Add more questions to quiz_id_1

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,7 +1,10 @@
 INSERT INTO questions (quiz_id, description, answer_one, answer_two, answer_three, answer_four, correct_answer)
 VALUES
 (1, 'What does the “19” in “COVID-19” refer to?', 'It''s the 19th variant', 'It''s the 19th pandemic', 'The name of a gene', 'The year it was discovered', 4),
-(1, 'Which viruses belong to the coronavirus family?', 'SARS and influenze', 'SARS and MERS', 'SARS and HIV', 'Other', 2),
+(1, 'Which viruses belong to the coronavirus family?', 'SARS and influenza', 'SARS and MERS', 'SARS and HIV', 'Other', 2),
+(1, 'How does COVID-19 spread?', 'Via droplets of air that we breathe', 'Via contaminated water', 'Via sexual fluid', 'Via food', 1),
+(1, 'How many coronaviruses are there?', '19', '4', 'Hundreds', 'Thousands', 3),
+(1, 'What are the common symptoms of COVID-19?', 'Cough', 'Fever', 'Tiredness', 'All of the above', 4),
 (2, 'When did the titanic sink?', '1901', '1812', '1912', '1982', 3),
 (2, 'Where did the Olympic Games originate?', 'Russia', 'Rome', 'Greece', 'England', 4),
 (3, 'What is the name of Harry''s owl?', 'Hedwig', 'Peeves', 'Nagini', 'Crookshanks', 1),
