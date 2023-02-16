6d35473add8de044eb915ef307004ebee7030013
EuphieSS <samma118@hotmail.com>
2023-02-16 06:06:18 +0000

    Add popup window for rating and score

--- public/scripts/quiz-id.js
+++ public/scripts/quiz-id.js
@@ -1,3 +1,32 @@
 $(document).ready(() => {
+  // $('#popup-container').hide();
+  // $('#overlay').hide();
+
+  $('#submit-btn').on('click', function() {
+    let score = 0;
+    $("input[type='radio']:checked").each(function() {
+      if ($("input[type='radio']:checked") === $("#correct-answers")) {
+        score++;
+      } else {
+        score+=0;
+      }
+    });
+
+    $('#score').text(score);
+
+    $('#popup-container').show();
+    $('#overlay').show();
+  });
+
+  $('#close-btn').on('click', function() {
+    $('#popup-container').hide();
+    $('#overlay').hide();
+  });
+
+  $("#rateYo").rateYo({
+    numStars: 4,
+    spacing: "10px"
+  });
+
 
 });
