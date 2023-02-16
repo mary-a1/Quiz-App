3e53e21b74ff9c9f84482cd67b14835ab3597fe6
Maryan <maryanay2000@gmail.com>
2023-02-16 17:18:33 -0500

    implement the result pages and styled

--- db/queries/get-results.js
+++ db/queries/get-results.js
@@ -3,15 +3,17 @@ const db = require('../connection');
 const getAllResults = function(quizId) {
   const queryParams = [quizId];
   let queryString = `
-  SELECT COUNT(*) AS score, users.name AS user
+  SELECT COUNT(*) AS score, users.name AS user, quizzes.title
   FROM questions AS questions
   JOIN results AS results
   ON question_id = questions.id
+  JOIN quizzes AS quizzes
+  ON quiz_id = quizzes.id
   JOIN users AS users
   ON player_id = users.id
   WHERE results.chosen_answer = questions.correct_answer AND
   questions.quiz_id = $1
-  GROUP BY users.id
+  GROUP BY users.id, quizzes.id
   ORDER BY score;`;
 
   return db.query(queryString, queryParams)
