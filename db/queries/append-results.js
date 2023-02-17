4e1d911648e3bbd1774d3eccf6a0b092fd4d3bd6
Irha Ali <irhaali_97@hotmail.ca>
2023-02-16 23:28:50 -0500

    IT WORKED!!!!!

---
+++ db/queries/append-results.js
@@ -0,0 +1,39 @@
+const db = require('../connection');
+const { addReview } = require('./add-review');
+
+const appendResult = function(playerId, review, answersSelected, questionIds) {
+  const queryParams = [playerId];
+  // Start the query
+  let queryString = `
+        INSERT INTO results (player_id, question_id, chosen_answer)
+        VALUES `;
+
+  // Add inserts
+  for (const [key, value] of Object.entries(answersSelected)) {
+    queryParams.push(key);
+    queryParams.push(value);
+    const queryParamsPosition = queryParams.length;
+    queryString += `($1, $${queryParamsPosition - 1}, $${queryParamsPosition}), `;
+  }
+
+  // End the query
+  queryString = `${queryString.slice(0, -2)}`;
+
+  db.query(queryString, queryParams);
+  return db.query(queryString, queryParams)
+    .then(() => {
+      const queryParams = [questionIds[0]];
+      const queryString = `SELECT quiz_id FROM questions WHERE id = $1`;
+      return db.query(queryString, queryParams);
+    })
+    .then((result) => {
+      const quizId = result.rows[0].quiz_id;
+      const queryParams = [quizId, playerId, review];
+      return addReview(queryParams);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+module.exports = { appendResult };
---
+++ db/queries/edit-results.js
@@ -0,0 +1,43 @@
+
+const db = require('../connection');
+const { addReview } = require('./add-review');
+
+const editResult = function(playerId, review, answersSelected, questionIds) {
+  const numberOfQuestions = questionIds.length;
+
+  let i = 0;
+  while (i < numberOfQuestions - 1) {
+    const key = questionIds[i];
+    const queryParams = [playerId, key, answersSelected.key];
+    let queryString = `
+            UPDATE results SET
+            player_id = $1,
+            question_id = $2,
+            chosen_answer = $3;`;
+    i++;
+    db.query(queryString, queryParams);
+  }
+  const key = questionIds[i];
+  const queryParams = [playerId, key, answersSelected.key];
+  let queryString = `
+          UPDATE results SET
+          player_id = $1,
+          question_id = $2,
+          chosen_answer = $3;`;
+  db.query(queryString, queryParams)
+    .then(() => {
+      const queryParams = [questionIds[0]];
+      const queryString = `SELECT quiz_id FROM questions WHERE id = $1`;
+      return db.query(queryString, queryParams);
+    })
+    .then((result) => {
+      const quizId = result.rows[0].quiz_id;
+      const queryParams = [quizId, playerId, review];
+      return addReview(queryParams);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+module.exports = { editResult };
